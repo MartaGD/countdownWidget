300469ac03ea4adfb694d1f162ece0a9a2461adc
MartaGD <marta.483@hotmail.com>
2024-03-16 17:36:09 +0100

    Date as parameter

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let selDate = "";
 
 function newCountdown(){
- selDate = "";
- document.getElementById("dateSelection-area").style.visibility = "visible";
- document.getElementById("countdown-area").style.visibility = "hidden";
+    let url = new URL(window.location.href);
+    url.searchParams.delete("selectedDate");
+    window.location.href = url;
+    selDate = "";
+    document.getElementById("countdown-area").style.visibility = "hidden";
+    document.getElementById("dateSelection-area").style.visibility = "visible";
 }
 
-function calendarChange(val){;
+function calendarChange(val){
     let dateText = document.getElementsByClassName("selected-date");    
     for (let i = 0; i < dateText.length; i++) {
         dateText[i].innerHTML = "Selected Date: " + val;
@@ -15,6 +18,19 @@ function calendarChange(val){;
 }
 
 function enterDate(){
+    let url = new URL(window.location.href);
+    url.searchParams.append("selectedDate",selDate);
+    window.location.href = url;
+    document.getElementById("dateSelection-area").style.visibility = "hidden";
+    document.getElementById("countdown-area").style.visibility = "visible";
+    //document.getElementById("selected-date").innerHTML = "Selected Date: " + val;
+    getCountdown();
+}
+
+function hasDate(){
+    let url = new URL(window.location.href);
+    url.searchParams.append("selectedDate",selDate);
+    //window.location.href = url;
     document.getElementById("dateSelection-area").style.visibility = "hidden";
     document.getElementById("countdown-area").style.visibility = "visible";
     //document.getElementById("selected-date").innerHTML = "Selected Date: " + val;
@@ -34,6 +50,21 @@ function getCountdown(){
     document.getElementById("countdownM").innerHTML = "Minutes: " + diffMinutes;
     document.getElementById("countdownS").innerHTML = "Seconds: " + diffSeconds;
 }
+
+function init() {  
+    let url = new URL(window.location.href);
+    if (url.searchParams.has("selectedDate")){
+        //alert("Tiene fecha informada "+url.searchParams.get("selectedDate"));
+        calendarChange(url.searchParams.get("selectedDate"));
+        hasDate();
+        alert("Copia esta URL en tu Notion: "+url);
+    }else{
+        //alert("No tiene fecha informada :(");
+    }
+    
+}
+ 
+window.onload = init();
 
 setInterval(getCountdown, 1000);
 getCountdown(selDate);
